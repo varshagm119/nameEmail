b46e84369e7793ba22705cea8a30458f583af291
varshagm119 <varshagm96@gmail.com>
2023-06-26 13:38:27 +0530

    adding local storage

---
+++ main.js
@@ -0,0 +1,31 @@
+const myForm=document.querySelector('#my-form');
+const nameInput=document.querySelector('#name');
+const emailInput=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+const userList=document.querySelector('#users');
+console.log('hi')
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value==='' || emailInput.value===''){
+
+        msg.classList.add('error');
+        msg.innerHTML='Please enter all fields';
+        setTimeout(() => msg.remove(),3000);
+
+    }
+    else{
+
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+        userList.appendChild(li);
+
+        localStorage.setItem('name',nameInput.value);
+        localStorage.setItem('email',emailInput.value);
+
+        nameInput.value='';
+        emailInput.value=''
+    }
+
+}
